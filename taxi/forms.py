d9b38bc578ad583d7d5ccca5722fa9b27cb3d8d2
Marharyta Marchenko <ritanika12@gmail.com>
2024-05-13 21:40:01 +0200

    'Solution'

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,48 +1,40 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.core.exceptions import ValidationError
-from django.core.validators import MaxLengthValidator
-
 from taxi.models import Driver, Car
 
 
-class DriverCreationForm(UserCreationForm):
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data.get("license_number")
+        if len(license_number) != 8:
+            raise forms.ValidationError(
+                "License number must be 8 characters long"
+            )
+        if (
+                not license_number[:3].isupper()
+                or not license_number[:3].isalpha()
+        ):
+            raise forms.ValidationError(
+                "First 3 characters must be uppercase"
+            )
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError(
+                "Last 5 characters must be digits"
+            )
+        return license_number
+
+
+class DriverCreationForm(UserCreationForm, DriverLicenseUpdateForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
         fields = UserCreationForm.Meta.fields + (
             "first_name", "last_name", "username", "license_number"
         )
-
-    def clean_license_number(self):
-        license_number = self.cleaned_data.get("license_number")
-
-        if len(license_number) != DriverLicenseUpdateForm.MAX_LENGTH:
-            raise ValidationError(
-                "License number must consist of 8 characters."
-            )
-
-        if not license_number[:3].isupper():
-            raise ValidationError(
-                "First 3 characters must be uppercase letters."
-            )
-
-        if not license_number[3:].isdigit():
-            raise ValidationError("Last 5 characters must be digits.")
-
-        return license_number
-
-
-class DriverLicenseUpdateForm(forms.ModelForm):
-    MAX_LENGTH = 8
-    license_number = forms.CharField(
-        required=True,
-        validators=[MaxLengthValidator(MAX_LENGTH)],
-    )
-
-    class Meta:
-        model = Driver
-        fields = ("license_number",)
 
 
 class CarForm(forms.ModelForm):
